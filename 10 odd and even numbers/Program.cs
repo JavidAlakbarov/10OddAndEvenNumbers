278ab15f021bf87d745f5d14f8682eec8b5fdd15
JavidAlakbarov <alakbarovjavid07@gmail.com>
2022-08-02 15:34:09 +0400

    sumoddandevennumbers

---
+++ 10 odd and even numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] oddnumbers = new int[10] {1,2,3,4,5,6,7,8,9,10 };
+int sum = 0;
+for (int i = 0; i < oddnumbers.Length; i++)
+{
+    if (oddnumbers[i] % 2 == 1)
+    {
+        sum += oddnumbers[i];
+    };
+}
+
+Console.WriteLine(sum);
